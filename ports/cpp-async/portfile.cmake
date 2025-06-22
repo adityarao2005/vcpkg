e38960f52594cad3e099569fa7efa29e72d37928
Aditya Rao <adityarao2005@gmail.com>
2025-06-22 00:20:01 -0400

    updated cpp-async fork version

--- ports/cpp-async/portfile.cmake
+++ ports/cpp-async/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO adityarao2005/cpp-async
     REF "v${VERSION}"
-    SHA512 7f694618eea87cd44f6fbd4a69b243adae0e011169b619bf8a8d5fbaf65f5a6d8a7b0913a339551bf04a41fee00b49b73596d9b4d8e85a8caba17d7c4220db81
+    SHA512 232f756417d6ca66da67df62034bde38a9f6d5408b02348aeffebe0eb2696395d7939cc6510feac44ce8c1ff5f4aa43c8205d6127f77bb9f8bde3babf28a3ffd
     HEAD_REF main
 )
 
